56ad19b6401f4a466c073cdc8056e17448632660
Sergey Grigoriev <s.a.grigoriev@gmail.com>
2014-01-18 20:10:14 +0200

    A test for java-based configuration.

--- src/test/java/org/springmock/JavaConfigTest.java
+++ src/test/java/org/springmock/JavaConfigTest.java
@@ -1,14 +1,32 @@
 package org.springmock;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
 /**
  * @author Sergey Grigoriev
  */
+@ContextConfiguration(classes = {JavaConfigTest.AppContext.class})
 public class JavaConfigTest extends AbstractTestNGSpringContextTests {
     @Test
     public void inject() {
+
+    }
+
+    @Configuration
+    static class AppContext {
+        @Bean
+        public MockInjectionPostProcessor mockInjectionPostProcessor() {
+             return new MockInjectionPostProcessor();
+        }
+
+        @Bean
+        public Service service() {
+            return new ServiceImpl();
+        }
 
     }
 }
