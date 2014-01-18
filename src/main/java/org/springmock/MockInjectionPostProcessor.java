33e39e2ed0dabb50104924d7544bd0751131b7f6
Sergey Grigoriev <s.a.grigoriev@gmail.com>
2014-01-18 19:59:37 +0200

    Cleanup.

--- src/main/java/org/springmock/MockInjectionPostProcessor.java
+++ src/main/java/org/springmock/MockInjectionPostProcessor.java
@@ -4,6 +4,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
@@ -14,7 +15,7 @@ import org.springframework.stereotype.Component;
  * @author Sergey Grigoriev
  */
 @Component
-public class MockInjectionPostProcessor implements BeanFactoryPostProcessor {
+public class MockInjectionPostProcessor implements BeanFactoryPostProcessor, DisposableBean {
     private static MockInjectionPostProcessor instance;
     private static final Map<String, Class> mocksByName = new LinkedHashMap<>();
 
@@ -34,5 +35,10 @@ public class MockInjectionPostProcessor implements BeanFactoryPostProcessor {
                 }
             }
         }
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        mocksByName.clear();
     }
 }
