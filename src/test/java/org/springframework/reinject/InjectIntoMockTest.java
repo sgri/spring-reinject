3356c87fe9f93ad3643b9e85856f041430f91da4
Sergey Grigoriev <s.a.grigoriev@gmail.com>
2014-02-02 21:41:45 +0200

    A pre-instantiated mock is not autowired #1

---
+++ src/test/java/org/springframework/reinject/InjectIntoMockTest.java
@@ -0,0 +1,50 @@
+package org.springframework.reinject;
+
+import javax.inject.Inject;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Sergey Grigoriev
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ReInjectContext.class, InjectIntoMockTest.InjectIntoMockTestContext.class})
+public class InjectIntoMockTest {
+    @Inject private Printer printer;
+
+    public InjectIntoMockTest() {
+        ReInjectPostProcessor.inject("printer", new Printer(){
+            @Inject private Service service;
+            @Override
+            public String print() {
+                Assert.assertNotNull(service);
+                return super.print();
+            }
+        });
+    }
+
+    @Test
+    public void injectIntoMock() {
+        printer.print();
+    }
+
+    @Configuration
+    static class InjectIntoMockTestContext {
+        @Bean
+        public Service service() {
+            return new ServiceImpl();
+        }
+
+        @Bean
+        public Printer printer() {
+            return new Printer();
+        }
+
+    }
+}
