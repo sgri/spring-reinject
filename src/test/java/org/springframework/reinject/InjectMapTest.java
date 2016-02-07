c495f7f9bd7c0d7dfd8e24f5fe18d8bd80143e9f
Sergey Grigoriev <s.a.grigoriev@gmail.com>
2016-02-07 13:05:52 +0200

    Simplification

--- src/test/java/org/springframework/reinject/InjectMapTest.java
+++ src/test/java/org/springframework/reinject/InjectMapTest.java
@@ -19,25 +19,27 @@ import static org.junit.Assert.assertEquals;
 @ContextConfiguration(locations = {"classpath:/org/springframework/reinject/reinject-context.xml", "classpath:/org/springframework/reinject/InjectMapTest-context.xml"})
 @DirtiesContext
 public class InjectMapTest {
-	
-	private static final String BEAN_NAME = "injectedMap";
 
-	private static final String STUB_KEY_1 = "MESA.KEY";
+    private static final String BEAN_NAME = "injectedMap";
 
-	private static final String STUB_VALUE_1 = "JarJar says hello";
+    private static final String STUB_KEY_1 = "MESA.KEY";
 
-	@Autowired @Qualifier(BEAN_NAME) MapFactoryBean injectedMap;
+    private static final String STUB_VALUE_1 = "JarJar says hello";
 
-	private Map<String, String> mapStub = new HashMap<>();
-	
-	public InjectMapTest() {
-		mapStub.put(STUB_KEY_1, STUB_VALUE_1);
-		ReInjectPostProcessor.inject(BEAN_NAME, Map.class, mapStub);
-	}
-	
-	@Test
-	public void injectMap() throws Exception {
-		assertEquals(mapStub, injectedMap.getObject());
-	}
+    @Autowired
+    @Qualifier(BEAN_NAME)
+    MapFactoryBean injectedMap;
+
+    private Map<String, String> mapStub = new HashMap<>();
+
+    public InjectMapTest() {
+        mapStub.put(STUB_KEY_1, STUB_VALUE_1);
+        ReInjectPostProcessor.inject(BEAN_NAME, mapStub);
+    }
+
+    @Test
+    public void injectMap() throws Exception {
+        assertEquals(mapStub, injectedMap.getObject());
+    }
 
 }
